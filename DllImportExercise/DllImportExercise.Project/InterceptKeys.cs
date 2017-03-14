07d6f4d7f136f87389b76f47e159b67ec10aa31b
Y2PM <joesamban@yahoo.co.uk>
2017-03-14 17:02:41 +0000

    layout

--- DllImportExercise/DllImportExercise.Project/InterceptKeys.cs
+++ DllImportExercise/DllImportExercise.Project/InterceptKeys.cs
@@ -38,22 +38,18 @@ class InterceptKeys
         }
         return CallNextHookEx(_hookID, nCode, wParam, lParam);
     }
-
+    //
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-
     private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
+    //
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-
     [return: MarshalAs(UnmanagedType.Bool)]
-
     private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
+    //
     [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-
     private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
+    //
     [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-
     private static extern IntPtr GetModuleHandle(string lpModuleName);
+    //
 }
